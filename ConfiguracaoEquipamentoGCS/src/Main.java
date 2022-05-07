fa7478311a86c1caf738ddc1fce4f67ee18a9e85
Gleyciane Brandão <gleyciane.brandao@furukawaelectric.com>
2022-05-07 10:03:28 -0300

    BR-GCS-02 Estrutura inicial do projeto

---
+++ ConfiguracaoEquipamentoGCS/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public void CadastrarEquipamento(){
+
+    }
+
+    public void EditarEquipamento(){
+
+    }
+
+    public void RemoverEquipamento(){
+
+    }
+
+    public void ValidarEquipamento(){
+
+    }
+
+    public void ConfigurarEquipamento(){
+
+    }
+
+}
